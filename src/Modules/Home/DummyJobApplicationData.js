7ab89ea4de10c6077ed8061b142e0a3e5df8597d
Ege <m.egegunduz@gmail.com>
2021-04-30 14:00:50 +0300

    Dummy application data

---
+++ src/Modules/Home/DummyJobApplicationData.js
@@ -0,0 +1,43 @@
+export default [
+    {
+        id: 1,
+        companyName: "Bilge Adam",
+        position: "Software Developer",
+        applicationDate: "30.04.2021",
+        URL: "www.site.com",
+        note: "Application notes will be shown here. This is kind of optional."
+    },
+    {
+        id: 2,
+        companyName: "Eti",
+        position: "R&D Engineer",
+        applicationDate: "30.04.2021",
+        URL: "www.site.com",
+        note: "Do I even really want this?"
+    },
+    {
+        id: 3,
+        companyName: "Şirket 3",
+        position: "Pozisyon 3",
+        applicationDate: "30.04.2021",
+        URL: "www.site.com",
+        note: ""
+    },
+    {
+        id: 4,
+        companyName: "Şirket 4",
+        position: "Pozisyon 4",
+        applicationDate: "30.04.2021",
+        URL: "www.site.com",
+        note: ""
+    },
+    {
+        id: 5,
+        companyName: "Şirket 5",
+        position: "Pozisyon 5",
+        applicationDate: "30.04.2021",
+        URL: "www.site.com",
+        note: ""
+    },
+
+]
